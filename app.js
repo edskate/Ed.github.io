55e25c7d227663a7f9d4fc1f295e1d50da5d8577
edilson gomes de lima <edskateicevida@gmail.com>
2023-09-05 12:12:43 -0300

    list

--- app.js
+++ app.js
@@ -2,3 +2,15 @@
 
 var minhaVariavel = "Olá, mundo!";
 console.log(minhaVariavel);
+
+let listaNumero = [1,2,3,4,5,6,7,8,9];
+console.log(listaNumero);
+
+// Como  adicionar ou alterar valores da lista
+
+let lista = ["be", "bebe", "bebece"];
+console.log(lista);
+
+// exibirá 3 
+// console.log(lista)
+
